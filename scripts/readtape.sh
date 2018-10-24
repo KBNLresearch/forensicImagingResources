a421124a87ff4480a4c930dcfe5728882dd28d1a
BitCurator <johan@crapstanzo.net>
2018-10-24 12:20:54 +0200

    expanded script

--- scripts/readtape.sh
+++ scripts/readtape.sh
@@ -14,45 +14,28 @@ TAPE=/dev/st0
 # Non-rewind tape device
 TAPEnr=/dev/nst0
 
-# Get tape status, output to array (split at newline)
-IFS=$'\n' tapeStatus=$(mt -f $TAPEnr status)
+# Initial block size
+bSize=128
+# Flag that indicates block size was found
+bSizeFound=false
+# Output file prefix
+prefix=test
+# File index
+index=1
+# Flag that indicates end of tape was reached
+endOfTape=false
+logFile=$prefix.log
 
-# Parse file number and block number from status output 
-for item in ${tapeStatus[*]}
-do
-    if [[ $item == *"file number"* ]]; then
-        # Split at equal sign, 2nd item is value
-        tmp=$(echo $item | cut -f2 -d=)
-        # Strip whitespace
-        fileNumber="$(echo -e "${tmp}" | tr -d '[:space:]')"
-        #echo $fileNumber
-    fi
-
-    if [[ $item == *"block number"* ]]; then
-        # Split at equal sign, 2nd item is value
-        tmp=$(echo $item | cut -f2 -d=)
-        # Strip whitespace
-        blockNumber="$(echo -e "${tmp}" | tr -d '[:space:]')"
-        #echo $blockNumber
-    fi
-
-done
-
-# TODO: status output probably not needed in the end,
-# investigate later.
+# Get tape status, append output to log file
+echo "*** Tape status ***" >> $logFile
+mt -f $TAPEnr status >> $logFile 2>&1
 
 # Determine the block size by trial and error
 
-# Initial block size
-bSize=128
-
-# Flag that indicates if block size was found
-bSizeFound=false
-
 while [ $bSizeFound == "false" ]
 do
-    # Try reading from tape
-    dd if=$TAPEnr of=tmp.dd ibs=$bSize count=1
+    # Try reading 1 block from tape
+    dd if=$TAPEnr of=/dev/null ibs=$bSize count=1
     ddStatus=$?
     if [[ $ddStatus -eq 0 ]]; then
         # dd exit status 0: block size found
@@ -63,23 +46,18 @@ do
     fi
 done
 
-echo $bSize
-echo $bSizeFound
-echo $?
+echo "*** Block size ***" >> $logFile
+echo $bSize >> $logFile
 
 # Rewind the tape
 mt -f $TAPEnr rewind
-
-# Output file prefix
-prefix=test
-endOfTape=false
-index=1
 
 while [ $endOfTape == "false" ]
 do
     # Read subsequent files until dd exit status not 0
     ofName=$prefix`printf "%06g" $index`.dd
-    dd if=$TAPEnr of=$ofName ibs=$bSize
+    echo "*** Processing file # "$index" ("$ofName") ***" >> $logFile
+    dd if=$TAPEnr of=$ofName ibs=$bSize >> $logFile 2>&1
     ddStatus=$?
     if [[ $ddStatus -eq 0 ]]; then
         # Increase index 
@@ -88,6 +66,13 @@ do
         # dd exit status not 0, reached end of tape
         # TODO: we also end up here if anything else goes wrong with dd,
         # might need further refinement
+        # Remove output file as it will be empty
+        rm $ofName
+        echo "*** Deleted "$ofName" (found end of tape) ***" >> $logFile
         endOfTape=true
     fi
 done
+
+# Rewind and eject the tape
+mt -f $TAPEnr rewind
+mt -f $TAPEnr eject
