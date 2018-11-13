2edddeaec70d297b0cb765eddf38f55b9e87e717
bitsgalore <johan.vanderknijff@kb.nl>
2018-11-13 13:56:27 +0100

    use tee for all output redirection

--- scripts/test-gui.sh
+++ scripts/test-gui.sh
@@ -34,11 +34,11 @@ findBlocksize ()
     while [ "$bSizeFound" == "false" ]
     do
         # Try reading 1 block from tape
-        echo "*** Guessing block size for session # ""$session"", trial value ""$bSize"" ***" >> "$logFile"
-        dd if="$tapeDevice" of=/dev/null bs="$bSize" count=1 >> "$logFile" 2>&1
+        echo "*** Guessing block size for session # ""$session"", trial value ""$bSize"" ***" | tee -a "$logFile"
+        dd if="$tapeDevice" of=/dev/null bs="$bSize" count=1 2>&1 | tee -a "$logFile"
         ddStatus="$?"
         # Position tape 1 record backward (i.e. to the start of this session)
-        mt -f "$tapeDevice" bsr 1 >> "$logFile" 2>&1
+        mt -f "$tapeDevice" bsr 1 2>&1 | tee -a "$logFile"
         if [[ "$ddStatus" -eq 0 ]]; then
             # dd exit status 0: block size found
             bSizeFound="true"
@@ -55,40 +55,40 @@ processSession ()
     if [ "$extractSession" = "true" ] ; then
         # Determine block size for this session
         findBlocksize
-        echo "*** Block size = ""$bSize"" ***" >> "$logFile"
+        echo "*** Block size = ""$bSize"" ***" | tee -a "$logFile"
 
         # Name of output file for this session
         ofName="$dirOut"/""$prefix""`printf "%06g" "$session"`."$extension"
 
-        echo "*** Extracting session # ""$session"" to file ""$ofName"" ***" >> "$logFile"
+        echo "*** Extracting session # ""$session"" to file ""$ofName"" ***" | tee -a "$logFile"
 
         if [ "$fill" = "TRUE" ] ; then
             # Invoke dd with conv=noerror,sync options
-            dd if="$tapeDevice" of="$ofName" bs="$bSize" conv=noerror,sync >> "$logFile" 2>&1
+            dd if="$tapeDevice" of="$ofName" bs="$bSize" conv=noerror,sync 2>&1 | tee -a "$logFile"
         else
-            dd if="$tapeDevice" of="$ofName" bs="$bSize" >> "$logFile" 2>&1
+            dd if="$tapeDevice" of="$ofName" bs="$bSize" 2>&1 | tee -a "$logFile"
         fi
 
         ddStatus="$?"
-        echo "*** dd exit code = " "$ddStatus"" ***" >> "$logFile"
+        echo "*** dd exit code = " "$ddStatus"" ***" | tee -a "$logFile"
     else
         # Fast-forward tape to next session
-        echo "*** Skipping session # ""$session"", fast-forward to next session ***" >> "$logFile"
-        mt -f "$tapeDevice" fsf 1 >> "$logFile" 2>&1
+        echo "*** Skipping session # ""$session"", fast-forward to next session ***" | tee -a "$logFile"
+        mt -f "$tapeDevice" fsf 1 2>&1 | tee -a "$logFile"
     fi
 
     # Try to position tape 1 record forward; if this fails this means
     # the end of the tape was reached
-    mt -f "$tapeDevice" fsr 1 >> "$logFile" 2>&1
+    mt -f "$tapeDevice" fsr 1 2>&1 | tee -a "$logFile"
     mtStatus="$?"
-    echo "*** mt exit code = " "$mtStatus"" ***" >> "$logFile"
+    echo "*** mt exit code = " "$mtStatus"" ***" | tee -a "$logFile"
 
     if [[ "$mtStatus" -eq 0 ]]; then
         # Another session exists. Position tape one record backward
-        mt -f "$tapeDevice" bsr 1 >> "$logFile" 2>&1
+        mt -f "$tapeDevice" bsr 1 2>&1 | tee -a "$logFile"
     else
         # No further sessions, end of tape reached
-        echo "*** Reached end of tape ***" >> "$logFile"
+        echo "*** Reached end of tape ***" | tee -a "$logFile"
         endOfTape="true"
     fi
 }
@@ -139,17 +139,17 @@ if [ -f "$logFile" ] ; then
 fi
 
 # Write some general info to log file
-echo "*** Tape extraction log ***" >> "$logFile"
+echo "*** Tape extraction log ***" | tee -a "$logFile"
 dateStart="$(date)"
-echo "*** Start date/time ""$dateStart"" ***" >> "$logFile"
-echo "*** Command-line arguments ***" >> "$logFile"
-echo "dirOut = ""$dirOut" >> "$logFile"
-echo "fill = ""$fill" >> "$logFile"
-echo "tapeDevice = ""$tapeDevice" >> "$logFile" 
-echo "blockSize = ""$blockSize" >> "$logFile"
-echo "sessions = ""$sessions" >> "$logFile"
-echo "prefix = ""$prefix" >> "$logFile"
-echo "extension = ""$extension" >> "$logFile"
+echo "*** Start date/time ""$dateStart"" ***" | tee -a "$logFile"
+echo "*** Command-line arguments ***" | tee -a "$logFile"
+echo "dirOut = ""$dirOut" | tee -a "$logFile"
+echo "fill = ""$fill" | tee -a "$logFile"
+echo "tapeDevice = ""$tapeDevice" | tee -a "$logFile" 
+echo "blockSize = ""$blockSize" | tee -a "$logFile"
+echo "sessions = ""$sessions" | tee -a "$logFile"
+echo "prefix = ""$prefix" | tee -a "$logFile"
+echo "extension = ""$extension" | tee -a "$logFile"
 
 # Check if block size is valid (i.e. a multiple of 512) by comparing integer
 # division of blockSize by 512 against floating-point division
@@ -168,7 +168,7 @@ if [ "$fill" = "TRUE" ] ; then
     # size is too large, so override user-defined value with default
     # if -f flag was used
     blockSize=512
-    echo "*** Reset blockSize to 512 because -f flag is used  ***" >> "$logFile"
+    echo "*** Reset blockSize to 512 because -f flag is used  ***" | tee -a "$logFile"
 fi
 
 # Flag that indicates end of tape was reached
@@ -177,8 +177,8 @@ endOfTape="false"
 session="1"
 
 # Get tape status, output to log file
-echo "*** Tape status ***" >> "$logFile"
-mt -f "$tapeDevice" status >> "$logFile"
+echo "*** Tape status ***" | tee -a "$logFile"
+mt -f "$tapeDevice" status | tee -a "$logFile"
 
 # Iterate over all sessions on tape until end is detected
 while [ "$endOfTape" == "false" ]
@@ -211,14 +211,14 @@ cd "$dirOut"
 checksumFile="$prefix"".sha512"
 sha512sum *."$extension" > "$checksumFile"
 cd "$workDir"
-echo "*** Created checksum file ***" >> "$logFile"
+echo "*** Created checksum file ***" | tee -a "$logFile"
 
 # Rewind and eject the tape
-echo "*** Rewinding tape ***" >> "$logFile"
-mt -f "$tapeDevice" rewind >> "$logFile" 2>&1
-echo "*** Ejecting tape ***" >> "$logFile"
-mt -f "$tapeDevice" eject >> "$logFile" 2>&1
+echo "*** Rewinding tape ***" | tee -a "$logFile"
+mt -f "$tapeDevice" rewind 2>&1 | tee -a "$logFile"
+echo "*** Ejecting tape ***" | tee -a "$logFile"
+mt -f "$tapeDevice" eject 2>&1 | tee -a "$logFile"
 
 # Write end date/time to log
 dateEnd="$(date)"
-echo "*** End date/time ""$dateEnd"" ***" >> "$logFile"
+echo "*** End date/time ""$dateEnd"" ***" | tee -a "$logFile"
