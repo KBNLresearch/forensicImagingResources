4951fa890af63fdae085a3875463b7f73159c7a5
bitsgalore <johan.vanderknijff@kb.nl>
2018-11-13 14:33:32 +0100

    reformatted logging message so they are recognised by yad

--- scripts/test-gui.sh
+++ scripts/test-gui.sh
@@ -34,7 +34,7 @@ findBlocksize ()
     while [ "$bSizeFound" == "false" ]
     do
         # Try reading 1 block from tape
-        echo "*** Guessing block size for session # ""$session"", trial value ""$bSize"" ***" | tee -a "$logFile"
+        echo "# Guessing block size for session # ""$session"", trial value ""$bSize" | tee -a "$logFile"
         dd if="$tapeDevice" of=/dev/null bs="$bSize" count=1 2>&1 | tee -a "$logFile"
         ddStatus="$?"
         # Position tape 1 record backward (i.e. to the start of this session)
@@ -55,12 +55,12 @@ processSession ()
     if [ "$extractSession" = "true" ] ; then
         # Determine block size for this session
         findBlocksize
-        echo "*** Block size = ""$bSize"" ***" | tee -a "$logFile"
+        echo "# Block size = ""$bSize" | tee -a "$logFile"
 
         # Name of output file for this session
         ofName="$dirOut"/""$prefix""`printf "%06g" "$session"`."$extension"
 
-        echo "*** Extracting session # ""$session"" to file ""$ofName"" ***" | tee -a "$logFile"
+        echo "# Extracting session # ""$session"" to file ""$ofName" | tee -a "$logFile"
 
         if [ "$fill" = "TRUE" ] ; then
             # Invoke dd with conv=noerror,sync options
@@ -70,10 +70,10 @@ processSession ()
         fi
 
         ddStatus="$?"
-        echo "*** dd exit code = " "$ddStatus"" ***" | tee -a "$logFile"
+        echo "# dd exit code = " "$ddStatus" | tee -a "$logFile"
     else
         # Fast-forward tape to next session
-        echo "*** Skipping session # ""$session"", fast-forward to next session ***" | tee -a "$logFile"
+        echo "# Skipping session # ""$session"", fast-forward to next session" | tee -a "$logFile"
         mt -f "$tapeDevice" fsf 1 2>&1 | tee -a "$logFile"
     fi
 
@@ -81,16 +81,106 @@ processSession ()
     # the end of the tape was reached
     mt -f "$tapeDevice" fsr 1 2>&1 | tee -a "$logFile"
     mtStatus="$?"
-    echo "*** mt exit code = " "$mtStatus"" ***" | tee -a "$logFile"
+    echo "# mt exit code = " "$mtStatus" | tee -a "$logFile"
 
     if [[ "$mtStatus" -eq 0 ]]; then
         # Another session exists. Position tape one record backward
         mt -f "$tapeDevice" bsr 1 2>&1 | tee -a "$logFile"
     else
         # No further sessions, end of tape reached
-        echo "*** Reached end of tape ***" | tee -a "$logFile"
+        echo "# Reached end of tape" | tee -a "$logFile"
         endOfTape="true"
     fi
+}
+
+processTape ()
+{
+    # Process a tape
+
+    # Write some general info to log file
+    echo "# Tape extraction log" | tee -a "$logFile"
+    dateStart="$(date)"
+    echo "# Start date/time ""$dateStart" | tee -a "$logFile"
+    echo "# Command-line arguments" | tee -a "$logFile"
+    echo "dirOut = ""$dirOut" | tee -a "$logFile"
+    echo "fill = ""$fill" | tee -a "$logFile"
+    echo "tapeDevice = ""$tapeDevice" | tee -a "$logFile" 
+    echo "blockSize = ""$blockSize" | tee -a "$logFile"
+    echo "sessions = ""$sessions" | tee -a "$logFile"
+    echo "prefix = ""$prefix" | tee -a "$logFile"
+    echo "extension = ""$extension" | tee -a "$logFile"
+
+    # Check if block size is valid (i.e. a multiple of 512) by comparing integer
+    # division of blockSize by 512 against floating-point division
+    blocksInt=$(($blockSize / 512))
+    blocksFloat=$(echo "$blockSize/512" | bc -l )
+    # This yields 1 if block size is valid, and 0 otherwise 
+    blocksizeValid=$(echo "$blocksInt == $blocksFloat" |bc -l)
+
+    if ! [ "$blocksizeValid" -eq 1 ] ; then
+        echo "ERROR: invalid blockSize, must be a multiple of 512!" >&2
+        exit 1
+    fi
+
+    if [ "$fill" = "TRUE" ] ; then
+        # dd's conv=sync flag results in padding bytes for each block if block 
+        # size is too large, so override user-defined value with default
+        # if -f flag was used
+        blockSize=512
+        echo "# Reset blockSize to 512 because -f flag is used " | tee -a "$logFile"
+    fi
+
+    # Flag that indicates end of tape was reached
+    endOfTape="false"
+    # Session index
+    session="1"
+
+    # Get tape status, output to log file
+    echo "# Tape status" | tee -a "$logFile"
+    mt -f "$tapeDevice" status | tee -a "$logFile"
+
+    # Iterate over all sessions on tape until end is detected
+    while [ "$endOfTape" == "false" ]
+    do
+        # Set initial value of extractSessionFlag depending on sessions parameter
+        if [ -z "$sessions" ] ; then
+            extractSession="true"
+        else
+            extractSession="false"
+        fi
+
+        # Only extract sessions defined by sessions parameter
+        # (if session parameter is empty all sessions are extracted)
+        for i in ${sessions//,/ }
+            do
+                if [ "$i" == "$session" ] ; then
+                    extractSession="true"
+                fi
+        done
+
+        # Call session processing function 
+        processSession
+        # Increase session number
+        let session="$session"+1
+    done
+
+    # Create checksum file
+    workDir="$PWD"
+    cd "$dirOut"
+    checksumFile="$prefix"".sha512"
+    sha512sum *."$extension" > "$checksumFile"
+    cd "$workDir"
+    echo "# Created checksum file" | tee -a "$logFile"
+
+    # Rewind and eject the tape
+    echo "# Rewinding tape" | tee -a "$logFile"
+    mt -f "$tapeDevice" rewind 2>&1 | tee -a "$logFile"
+    echo "# Ejecting tape" | tee -a "$logFile"
+    mt -f "$tapeDevice" eject 2>&1 | tee -a "$logFile"
+
+    # Write end date/time to log
+    dateEnd="$(date)"
+    echo "# End date/time ""$dateEnd" | tee -a "$logFile"
 }
 
 # **************
@@ -138,87 +228,17 @@ if [ -f "$logFile" ] ; then
     rm "$logFile"
 fi
 
-# Write some general info to log file
-echo "*** Tape extraction log ***" | tee -a "$logFile"
-dateStart="$(date)"
-echo "*** Start date/time ""$dateStart"" ***" | tee -a "$logFile"
-echo "*** Command-line arguments ***" | tee -a "$logFile"
-echo "dirOut = ""$dirOut" | tee -a "$logFile"
-echo "fill = ""$fill" | tee -a "$logFile"
-echo "tapeDevice = ""$tapeDevice" | tee -a "$logFile" 
-echo "blockSize = ""$blockSize" | tee -a "$logFile"
-echo "sessions = ""$sessions" | tee -a "$logFile"
-echo "prefix = ""$prefix" | tee -a "$logFile"
-echo "extension = ""$extension" | tee -a "$logFile"
+# Call main processing function. All logging output is redirected
+# to a yad text-info widget
+#processTape  | yad --text-info \
+#    --width=400 --height=600 \
+#    --title="Progress" \
+#    --scroll \
+#    --no-buttons
 
-# Check if block size is valid (i.e. a multiple of 512) by comparing integer
-# division of blockSize by 512 against floating-point division
-blocksInt=$(($blockSize / 512))
-blocksFloat=$(echo "$blockSize/512" | bc -l )
-# This yields 1 if block size is valid, and 0 otherwise 
-blocksizeValid=$(echo "$blocksInt == $blocksFloat" |bc -l)
-
-if ! [ "$blocksizeValid" -eq 1 ] ; then
-    echo "ERROR: invalid blockSize, must be a multiple of 512!" >&2
-    exit 1
-fi
-
-if [ "$fill" = "TRUE" ] ; then
-    # dd's conv=sync flag results in padding bytes for each block if block 
-    # size is too large, so override user-defined value with default
-    # if -f flag was used
-    blockSize=512
-    echo "*** Reset blockSize to 512 because -f flag is used  ***" | tee -a "$logFile"
-fi
-
-# Flag that indicates end of tape was reached
-endOfTape="false"
-# Session index
-session="1"
-
-# Get tape status, output to log file
-echo "*** Tape status ***" | tee -a "$logFile"
-mt -f "$tapeDevice" status | tee -a "$logFile"
-
-# Iterate over all sessions on tape until end is detected
-while [ "$endOfTape" == "false" ]
-do
-    # Set initial value of extractSessionFlag depending on sessions parameter
-    if [ -z "$sessions" ] ; then
-        extractSession="true"
-    else
-        extractSession="false"
-    fi
-
-    # Only extract sessions defined by sessions parameter
-    # (if session parameter is empty all sessions are extracted)
-    for i in ${sessions//,/ }
-        do
-            if [ "$i" == "$session" ] ; then
-                extractSession="true"
-            fi
-    done
-
-    # Call session processing function 
-    processSession
-    # Increase session number
-    let session="$session"+1
-done
-
-# Create checksum file
-workDir="$PWD"
-cd "$dirOut"
-checksumFile="$prefix"".sha512"
-sha512sum *."$extension" > "$checksumFile"
-cd "$workDir"
-echo "*** Created checksum file ***" | tee -a "$logFile"
-
-# Rewind and eject the tape
-echo "*** Rewinding tape ***" | tee -a "$logFile"
-mt -f "$tapeDevice" rewind 2>&1 | tee -a "$logFile"
-echo "*** Ejecting tape ***" | tee -a "$logFile"
-mt -f "$tapeDevice" eject 2>&1 | tee -a "$logFile"
-
-# Write end date/time to log
-dateEnd="$(date)"
-echo "*** End date/time ""$dateEnd"" ***" | tee -a "$logFile"
+processTape | yad --progress \
+    --width=400 --height=600 \
+    --title="Progress" \
+    --scroll \
+    --enable-log \
+    --log-expanded
