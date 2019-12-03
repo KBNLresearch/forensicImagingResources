92b712992ec26043e6af306adfb03988f0517693
bitsgalore <johan.vanderknijff@kb.nl>
2019-12-03 17:48:14 +0100

    added auto-identification of container format, extraction of tar files and also call restore in non-interactive mode

---
+++ scripts/extractcontainer.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+# Extract contents of dump and tar containers
+# for dump files this requires some user input,
+# and permissions are fixed on output afterwards
+# Script must be run with sudo privilege
+
+if [ "$#" -ne 2 ] ; then
+  echo "Usage: extractcontainer.sh fileIn dirOut" >&2
+  exit 1
+fi
+
+if ! [ -f "$1" ] ; then
+  echo "fileIn must be a file" >&2
+  exit 1
+fi
+
+if ! [ -d "$2" ] ; then
+  echo "dirOut must be a directory" >&2
+  exit 1
+fi
+
+# Input file
+fileIn="$1"
+
+# Establish file type
+fTypeString=$(file $fileIn)
+
+if [[ $fTypeString == *"new-fs dump file"* ]]; then
+  fileType="dump"
+fi
+
+if [[ $fTypeString == *"tar archive"* ]]; then
+  fileType="tar"
+fi
+
+echo $fileType
+
+# Output directory (absolute path)
+dirOut=$(realpath "$2")
+
+# User directory (absolute path)
+dirUser=$(pwd)
+
+# Go to output directory
+cd $dirOut
+
+if [[ $fileType == "dump" ]]; then
+  echo "Extracting dump file ..."
+  # Source: https://docs.oracle.com/cd/E19253-01/817-5093/bkuprestoretasks-72504/index.html
+  restore -xvf $fileIn .
+  # Go back to user directory
+  cd $dirUser
+
+  echo "Fixing permissions on extracted files and directories ..."
+  find $dirOut -type f -exec chmod 644 {} \;
+  find $dirOut -type d -exec chmod 755 {} \;
+
+fi
+
+if [[ $fileType == "tar" ]]; then
+  echo "Extracting TAR archive ..."
+  # Run tar
+  tar -xf $fileIn
+
+  # Go back to user directory
+  cd $dirUser
+
+  #echo "Fixing permissions on extracted files and directories ..."
+  #find $dirOut -type f -exec chmod 644 {} \;
+  #find $dirOut -type d -exec chmod 755 {} \;
+fi
+
+# Notify user
+echo "Done!"
